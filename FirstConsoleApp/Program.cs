22c525d2fde85846033108d0c1cc2d0b79f6be6d
DELL <DELL@DANIEL>
2023-06-07 13:55:10 +0100

    Add project files.

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Program start here
+/* This is to print Hello World
+ using console
+
+ */
+
+
+Console.WriteLine("Hello, World!");
+Console.ReadLine();
+
+
+//Declaration of variables
+string studentname;
+int studentAge;
+bool studentpresent;
+char studentSex;
+double studentFee;
+
+//Initializing or assigning values to the variables
+studentname = "Daniel";
+studentAge = 13;
+studentpresent = true;
+studentSex = 'M';
+studentFee = 500000.45;
+
+//Basic operations with the variables (+,-,*,/,%)
+//Addition
+studentAge += 23;
+Console.WriteLine("The new age is "+ studentAge + "Years");
+
+Console.ReadLine();
+
